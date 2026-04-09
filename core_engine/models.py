14e11982b20a2ac188d84fccce24be5ee97c4f0a
AUOR17 <angel_osorno@outlook.com>
2026-04-09 13:56:03 -0600

    feat: Added core modules and test modules
    - Create a main file to execute all the modules.
    - Create a test file for ensure the communication between the modules

---
+++ core_engine/models.py
@@ -0,0 +1,90 @@
+class Personaje:
+
+    def __init__(self, nombre, salud_maxima):
+        self.nombre = nombre
+        self._salud = salud_maxima
+        self.salud_maxima = salud_maxima
+
+    @property
+    def salud(self):
+        return self._salud
+    
+    @salud.setter
+    def salud(self, nueva_salud: int) -> int:
+
+        if nueva_salud < 0:
+            self._salud = 0
+            print(f"{self.nombre} ha muerto.")
+        elif nueva_salud > self.salud_maxima:
+            self._salud = self.salud_maxima
+        else:
+            self._salud = nueva_salud
+
+    def __str__(self):
+        return f"{self.nombre} | HP: {self.salud}/{self.salud_maxima}"
+    
+class Guerrero(Personaje):
+
+    def __init__(self, nombre, salud_maxima, puntos_armadura):
+        super().__init__(nombre, salud_maxima) 
+        self.puntos_armadura = puntos_armadura
+
+    def __str__(self):
+        return "Guerrero: " + super().__str__() + f" | Armadura: {self.puntos_armadura}" 
+    
+class Mago(Personaje): 
+
+    def __init__(self, nombre, salud_maxima, mana_maximo):
+        super().__init__(nombre, salud_maxima)
+        self.mana_maximo = mana_maximo
+        self._mana = mana_maximo
+
+    def __str__(self):
+        return "Mago: " + super().__str__() + f" | Mana: {self._mana}/{self.mana_maximo}" 
+    
+    def lanzar_hechizo(self,costo_mana: int) -> None:
+
+        if self._mana >= costo_mana:
+            self._mana -= costo_mana
+            print(f"{self.nombre} lanza un hechizo!. Mana restante: {self._mana}/{self.mana_maximo}")
+        else:
+            print(f"{self.nombre} no tiene suficiente mana para atacar. ")
+
+class JefeFinal(Personaje):
+
+    def __init__(self, nombre, salud_maxima, multiplocador_damage):
+        super().__init__(nombre, salud_maxima)
+        self.mutiplicador_damage = multiplocador_damage
+        self.enfurecido = False
+
+    def __str__(self):
+        estado = "Enfurecido" if self.enfurecido else "Acechando"
+        return f"Jefe Final: {super().__str__()} | Daño: {self.mutiplicador_damage} | Estado: {estado}"
+    
+    def recibir_daño_critico(self,cantidad):
+        self.salud -= cantidad
+        if 0 < self.salud <= (self.salud_maxima * 0.3) and not self.enfurecido:
+            self.enfurecido = True
+            self.mutiplicador_damage *= 2
+            print(f" El cielo oscurece, {self.nombre} ha entrado en FASE 2")
+            print(f"Su multiplicador de daño subio a {self.mutiplicador_damage}")
+
+class Gremio: 
+
+    def __init__(self, nombre_gremio):
+        self.nombre_gremio = nombre_gremio
+        self.miembros = []
+    
+    def reclutar_miembros(self, personaje):
+        self.miembros.append(personaje)
+        print(f"{personaje.nombre} se ha unido al gremio {self.nombre_gremio}")
+
+    def listar_miembros(self):
+
+        print(f"--- Gremio {self.nombre_gremio.upper()} ---")
+        if not self.miembros:
+            print("El gremio esta vacio, Recluta a alguien")
+        else:
+            for miembro in self.miembros:
+                print(miembro)
+        print("-" * 30)
---
+++ tests/test_core.py
@@ -0,0 +1,84 @@
+import pytest
+import sys
+import os 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from core_engine.models import Guerrero, Mago, JefeFinal, Gremio
+from core_engine.database import inicializar_db, obtener_conexion
+
+@pytest.fixture()
+def db_limpia():
+
+    nombre_db = 'test_partida.db'
+    if os.path.exists(nombre_db):
+        os.remove(nombre_db)
+
+    inicializar_db(nombre_db)
+
+    yield nombre_db
+
+    if os.path.exists(nombre_db):
+        os.remove(nombre_db)
+
+def test_reglas_salud():
+    kratos = Guerrero("kratos", salud_maxima=100, puntos_armadura=90)
+
+    kratos.salud = -50000
+    assert kratos.salud == 0
+
+    kratos.salud = 999999
+    assert kratos.salud == 100
+
+@pytest.mark.parametrize("personaje, nombre, salud_maxima, atributo_extra",[
+    (Guerrero, 'Arthur',300,50),
+    (Mago, 'Merlin', 50, 140),
+    (JefeFinal, 'Sauron', 1000, 1.5)
+])
+def test_creacion_personaje(personaje, nombre, salud_maxima, atributo_extra):
+    personaje_creado = personaje(nombre, salud_maxima, atributo_extra)
+    assert personaje_creado.nombre == nombre
+    assert personaje_creado.salud_maxima == salud_maxima
+    
+    if isinstance(personaje_creado, Mago):
+        assert personaje_creado.mana_maximo == 140
+    elif isinstance(personaje_creado, JefeFinal):
+        assert personaje_creado.enfurecido == False
+
+
+def test_database_guardar_gremio(db_limpia):
+    try:
+        nombre_db_prueba = db_limpia
+        gremio_nuevo = Gremio('Fairy Tail')
+        natsu = Mago('Natsu', 150, 300)
+
+        conexion = obtener_conexion(nombre_db_prueba)
+        cursor = conexion.cursor()
+
+        cursor.execute("INSERT INTO gremios (nombre_gremio) VALUES (?)", (gremio_nuevo.nombre_gremio,))
+        gremio_id = cursor.lastrowid
+
+        cursor.execute('''
+            INSERT INTO personajes (nombre, salud_maxima, tipo_clase, mana_maximo, gremio_id)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (natsu.nombre, natsu.salud_maxima, 'Mago', natsu.mana_maximo, gremio_id))
+
+        conexion.commit()
+
+        cursor.execute('''
+            SELECT p.nombre, g.nombre_gremio
+            FROM personajes p
+            JOIN gremios g on p.gremio_id = g.id
+            where p.nombre = 'Natsu'
+        ''')
+        resultado = cursor.fetchone()
+
+        print(resultado)
+
+        assert resultado is not None, "El personaje NO se guardo en la DB"
+        assert resultado[0] == 'Natsu', 'El nombre guardado es incorrecto'
+        assert resultado[1] == 'Fairy Tail', 'El gremio guardado es incorrecto'
+
+    finally:
+        conexion.close()
+    
