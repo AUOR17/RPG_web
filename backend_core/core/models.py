e46294c31d4dd08d96846d762a4b5c365aace2e1
AUOR17 <angel_osorno@outlook.com>
2026-04-25 15:01:11 -0600

    Added confs

---
+++ backend_core/core/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        GUERRERO = 'GUERRERO', 'Guerrero (Ventas)'
+        MAGO = 'MAGO', 'Mago (Marketing)'
+        PICARO = 'PICARO', 'Pícaro (Operaciones)'
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.GUERRERO)
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+    gold = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.username} - Nivel {self.level} {self.role}"
